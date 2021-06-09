fb4c7344b363dcf4de83cc17fed2ddcf15e02eed
lilian-83 <47923350+lilian-83@users.noreply.github.com>
2021-06-09 11:38:22 +0200

    Update data_PTP_regime.m

--- data_PTP_regime.m
+++ data_PTP_regime.m
@@ -6,9 +6,9 @@ tic
 
 m=150;
 nb=20;
-grids=200;
+grids=100;
 
-[g1,G1,Q1,acc1]=solve_problems_regime(m,nb,grids);
+[g1,G1,Q1,acc1]=solve_problems_regime_par(m,nb,grids);
 
 save("results_regime.mat");
 
